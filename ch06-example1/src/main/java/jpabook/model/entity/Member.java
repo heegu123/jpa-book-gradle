bfffe917f7278b4bae36c02d2559e312451ac1f7
heegu123 <kookminkhg@kookmin.ac.kr>
2025-04-21 13:38:09 +0900

    ch06 예제 1 추가 ch06 model3 추가

---
+++ ch06-example1/src/main/java/jpabook/model/entity/Member.java
@@ -0,0 +1,43 @@
+package jpabook.model.entity;
+
+import jakarta.persistence.*;
+
+//N : 1 양방향
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
