56ca5ee7d72639132977d6b1710fd2d86c54983b
heegu123 <kookminkhg@kookmin.ac.kr>
2025-04-25 23:12:15 +0900

    ch07 예제-5 @IdClass 예제 추가

--- ch07-example5-idclass/src/main/java/jpabook/model/Main.java
+++ ch07-example5-idclass/src/main/java/jpabook/model/Main.java
@@ -4,8 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jpabook.model.entity.Member;
-import jpabook.model.entity.Seller;
+import jpabook.model.entity.Child;
+import jpabook.model.entity.Parent;
+import jpabook.model.entity.ParentId;
 
 /**
  * Created by 1001218 on 15. 4. 5..
@@ -21,17 +22,21 @@ public class Main {
         try {
             tx.begin();
 
-            Member member = new Member();
-            member.setName("김민수");
-            member.setEmail("example@example.com");
-            em.persist(member);
+            Parent parent = new Parent();
+            parent.setId1("myId1");
+            parent.setId2("myId2");
+            parent.setName("parentName");
+            em.persist(parent);
 
-            Seller seller = new Seller();
-            seller.setName("판매자1");
-            seller.setShopName("매장이름1");
-            em.persist(seller);
+            Child child = new Child();
+            child.setParent(parent);
+            em.persist(child);
 
             tx.commit();
+
+            ParentId parentId = new ParentId("myId1", "myId2");
+            Parent findParent = em.find(Parent.class, parentId);
+            System.out.println("findParent.equals(parent) = " + findParent.equals(parent));
 
         } catch (Exception e) {
             e.printStackTrace();
